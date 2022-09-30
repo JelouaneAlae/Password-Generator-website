089643c48b64da3f6ef7416859e991b39748996f
jelouanealae <jel.alae37@gmail.com>
2022-09-30 12:26:17 +0100

    init

---
+++ Jscript.js
@@ -0,0 +1,40 @@
+function generatPass(){
+    document.getElementById("txtresult").value= null;
+    var lenght = document.getElementById("range").value;
+    var Upper = false;
+    var Symbol = false;
+    var number =false;
+    var lettre = "abcdefghijklmnopqrstuvwxyz";
+    var password = ""; 
+    if(document.getElementById("checkboxUpper").checked){
+        Upper = true;
+    }
+    if(document.getElementById("checkboxSymbol").checked){
+        Symbol = true;
+    }
+    if(document.getElementById("checkboxNumber").checked){
+        number = true;
+    }
+    if(Upper == true){
+         lettre += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if(Symbol == true){
+        lettre +="!@#$%^&*()";
+    }
+    if(Symbol == true){
+        lettre +="0123456789";
+    }
+
+    // for (var i =0 ; i < range;i++  ){
+    //     randomNumber= Math.floor(Math.random* lettre) ;
+    //     password += chars.substring(randomNumber, randomNumber +1);
+    // }
+    // document.getElementById("txtresult").value = password;
+
+    password = Math.floor(Math.random());
+    document.getElementById("txtresult").value=password;
+
+}
+
+
+
